ea3cefea59c4fe5b4089595b71cd59a065a8c1e9
jessicagrayson <grayjessica12@hotmail.com>
2023-08-02 16:45:25 +0000

    completed user-can-create-an-entry feature

--- js/data.js
+++ js/data.js
@@ -8,11 +8,20 @@ let data = {
 };
 
 // function to serialize and then store form data as JSON
-function serialzeData(event) {
-  const dataString = JSON.stringify('data');
+function serializeData(event) {
+  const dataString = JSON.stringify(data);
   const dataStorageKey = 'serializedData';
 
   localStorage.setItem(dataStorageKey, dataString);
 }
 
-window.addEventListener('beforeunload', serialzeData);
+window.addEventListener('beforeunload', serializeData);
+
+// conditional statement to parse JSON data if present and assign to data variable
+
+const dataStorageKey = 'serializedData';
+const storedDataString = localStorage.getItem(dataStorageKey);
+
+if (storedDataString) {
+  data = JSON.parse(storedDataString);
+}
