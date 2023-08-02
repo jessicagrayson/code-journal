e8e7cc14bb7d8534af6a7998615028af3144d53a
jessicagrayson <grayjessica12@hotmail.com>
2023-08-02 19:38:31 +0000

    created function renderEntry that will render and return a DOM tree for each journal entry

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ function submitInfo(event) {
   const notes = $userNotes.value;
   // assigns form values to new object
   const formData = {
+    // this is using shorthand that allows you to just type out the name one time if your key and value are called the same thing.
     title,
     url,
     notes,
@@ -42,3 +43,36 @@ function submitInfo(event) {
 
 // submit event listener
 $entryForm.addEventListener('submit', submitInfo);
+
+// function to render entries
+
+// eslint-disable-next-line no-unused-vars
+function renderEntry(entry) {
+  // builds elements of DOM tree:
+  const $row = document.createElement('div');
+  $row.className = 'row';
+  // create column (may need to change class name)
+  const $column = document.createElement('div');
+  $column.className = 'column-full';
+  const $list = document.createElement('ul');
+  $list.className = 'journal-entry';
+  const $listItem = document.createElement('li');
+  $listItem.className = 'user-entry';
+  // possible need for a div or container here, think $pokemonCard div
+  const $img = document.createElement('img');
+  // may need to change image value
+  $img.setAttribute('src', $currentPhotoUrl);
+  const $title = document.createElement('h2');
+  $title.className = 'entry-title';
+  const $notes = document.createElement('p');
+  $notes.className = 'entry-notes';
+
+  // append DOM nodes
+
+  $row.appendChild($column);
+  $column.appendChild($list);
+  $list.appendChild($listItem);
+  $listItem.appendChild($img);
+  $listItem.appendChild($title);
+  $listItem.appendChild($notes);
+}
