e5c6c0960420f374eb54c5d9346c4b55f7ee2e72
jessicagrayson <grayjessica12@hotmail.com>
2023-08-02 16:30:23 +0000

    Testing if prettier works

--- js/main.js
+++ js/main.js
@@ -17,22 +17,22 @@ function submitInfo(event) {
   event.preventDefault();
 
   // gets form values:
-  const $title = $currentTitle.value;
-  const $photoUrl = $currentPhotoUrl.value;
-  const $notes = $userNotes.value;
+  const title = $currentTitle.value;
+  const url = $currentPhotoUrl.value;
+  const notes = $userNotes.value;
   // assigns form values to new object
-  const $formData = {
-    title: $title,
-    url: $photoUrl,
-    notes: $notes,
+  const formData = {
+    title,
+    url,
+    notes,
   };
   // assigns value of nextEntryId property of data object to new property entryId and adds to $formData
-  $formData.entryId = data.nextEntryId;
+  formData.entryId = data.nextEntryId;
   // // increments the nextEntryId property of the data model
   data.nextEntryId = data.nextEntryId + 1;
 
   // adds object to beginning of array
-  data.entries.unshift($formData);
+  data.entries.unshift(formData);
 
   // Resets image src
   $userCurrentImg.src = 'images/placeholder-image-square.jpg';
@@ -42,3 +42,10 @@ function submitInfo(event) {
 
 // submit event listener
 $entryForm.addEventListener('submit', submitInfo);
+
+function test() {
+  const test = 'Some string';
+  localStorage.setItem('random', test);
+}
+
+test();
