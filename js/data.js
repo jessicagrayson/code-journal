60670b16927fc8fc2747e9d8d491e131a0a0e384
jessicagrayson <grayjessica12@hotmail.com>
2023-08-02 06:36:22 +0000

    added functionality that will serialize and store form data as JSON in localStorage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+// function to serialize and then store form data as JSON
+function serialzeData(event) {
+  const dataString = JSON.stringify('data');
+  const dataStorageKey = 'serializedData';
+
+  localStorage.setItem(dataStorageKey, dataString);
+}
+
+window.addEventListener('beforeunload', serialzeData);
